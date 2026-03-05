3cb5333c6d07314a134093d317af6d4f005322fe
Stephen Wood <swoo@Mac.fritz.box>
2026-03-05 20:50:03 +0100

    🎨 chore(storybook): migrate preview to use Emotion globalStyles decorator

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,22 @@
-import '../src/index.css';
-
+import { Global } from '@emotion/react';
 import type { Preview } from '@storybook/react-vite';
+import React from 'react';
+
+import { globalStyles } from '../src/theme/GlobalStyles';
 
 const preview: Preview = {
+	decorators: [
+		(Story) => (
+			<>
+				<Global styles={globalStyles} />
+				<Story />
+			</>
+		),
+	],
 	parameters: {
 		a11y: {
-			// 'todo' - show a11y violations in the test UI only
-			// 'error' - fail CI on a11y violations
-			// 'off' - skip a11y checks entirely
 			test: 'todo',
 		},
-
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
