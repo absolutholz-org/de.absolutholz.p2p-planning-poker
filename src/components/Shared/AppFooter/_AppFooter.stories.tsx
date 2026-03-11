6fcd8bdaf6b30eb25d7790ea295a2471f8002fcf
Stephen Wood <swoo@MAC0022.local>
2026-03-11 16:01:03 +0100

    🎨 chore: add Storybook for AppFooter component

---
+++ src/components/Shared/AppFooter/_AppFooter.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import React, { useEffect } from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import i18n from '../../../i18n/config';
+import { AppFooter } from './_AppFooter';
+
+const meta = {
+	component: AppFooter,
+	decorators: [
+		(Story) => (
+			<MemoryRouter>
+				<Story />
+			</MemoryRouter>
+		),
+	],
+	parameters: {
+		layout: 'fullscreen',
+	},
+	tags: ['autodocs'],
+	title: 'Shared/AppFooter',
+} satisfies Meta<typeof AppFooter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const German: Story = {
+	decorators: [
+		(Story) => {
+			useEffect(() => {
+				i18n.changeLanguage('de');
+			}, []);
+			return <Story />;
+		},
+	],
+};
+
+export const English: Story = {
+	decorators: [
+		(Story) => {
+			useEffect(() => {
+				i18n.changeLanguage('en');
+			}, []);
+			return <Story />;
+		},
+	],
+};
+
+export const PortugueseFallback: Story = {
+	decorators: [
+		(Story) => {
+			useEffect(() => {
+				i18n.changeLanguage('pt');
+			}, []);
+			return <Story />;
+		},
+	],
+};
