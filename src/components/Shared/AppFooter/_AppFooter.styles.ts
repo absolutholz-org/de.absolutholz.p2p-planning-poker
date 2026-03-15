f4d7f07f4911e4440072636fa2258dc2aed9ff93
Stephen Wood <swoo@Mac.fritz.box>
2026-03-15 11:09:21 +0100

    💄 style: add divider to AppFooter and enable link wrapping

--- src/components/Shared/AppFooter/_AppFooter.styles.ts
+++ src/components/Shared/AppFooter/_AppFooter.styles.ts
@@ -2,9 +2,11 @@ import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
 export const FooterContainer = styled.footer`
+	border-top: 1px solid var(--sys-color-border);
 	padding: var(--sys-spacing-md);
 	display: flex;
 	justify-content: center;
+	flex-wrap: wrap;
 	gap: var(--sys-spacing-lg);
 	background-color: transparent;
 `;
